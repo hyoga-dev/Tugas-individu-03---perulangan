dd56facc89126fcbe72521779e10a5f88208e315
Lamy-lame <75929898+hyoga-dev@users.noreply.github.com>
2024-10-14 11:03:57 +0700

    Update GanjilGenap.cpp

--- GanjilGenap.cpp
+++ GanjilGenap.cpp
@@ -5,7 +5,7 @@ int main()
 {
     int data, ganjil = 0, genap = 0;
     cout << "Masukkan jumlah data: "; cin >> data;
-    for (int a = 0; a <= data; a++) (a % 2 == 0) ? ganjil += a : genap += a; 
+    for (int a = 0; a <= data; a++) (a % 2 == 0) ? genap += a : ganjil += a; 
     cout << "Jumlah total bilangan ganjil: " << ganjil 
          << "\nJumlah total bilangan genap: " << genap;
 
